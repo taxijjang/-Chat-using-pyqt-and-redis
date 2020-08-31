5202e56622a0ff40cbc44126e7f373cfaf77ff9b
taxijjang <gw9122@Naver.com>
2020-08-31 18:00:06 +0900

    일단 코드 대충 만들어서 다시 수정 해야됨

--- pyqt.py
+++ pyqt.py
@@ -6,7 +6,7 @@ import time
 import socket
 import redis
 
-#from .redis_connect import redis_connect as rc
+# from .redis_connect import redis_connect as rc
 
 form_class = uic.loadUiType("chat.ui")[0]
 
@@ -14,9 +14,12 @@ form_class = uic.loadUiType("chat.ui")[0]
 ### redis에 저장되어 있는 채팅목록 가지고 오기 ###
 class Worker(QThread):
     finished = pyqtSignal(str)
-    user_r = redis.Redis(host='3.34.134.147', port=6379, db=0)
-    user_pub_sub = user_r.pubsub()
-    user_pub_sub.subscribe('chat_server')
+
+    def __init__(self, sec=0, parent=None):
+        super().__init__()
+        self.user_r = redis.Redis(host='3.34.134.147', port=6379, db=0)
+        self.user_pub_sub = self.user_r.pubsub()
+        self.user_pub_sub.subscribe('chat_server')
 
     def run(self):
 
@@ -24,11 +27,11 @@ class Worker(QThread):
             message = self.user_pub_sub.get_message()
 
             if message:
-                msg = str(message['data'])
+                msg = str(message['data'])[1:]
                 self.finished.emit(msg)
                 print(msg)
 
-            time.sleep(1)
+            time.sleep(0.01)
 
 
 class MyChat(QMainWindow, form_class):
@@ -45,14 +48,21 @@ class MyChat(QMainWindow, form_class):
 
         self.input_text.returnPressed.connect(self.append_text)
 
+        self.user_r = redis.Redis(host='3.34.134.147', port=6379, db=0)
+        self.user_r.pubsub()
+
     def append_text(self):
         text = self.input_text.text()
         message = str(self.my_ip_addr + " : " + text)
-        self.text_browser.append(message)
+        #self.text_browser.append(message)
+        self.user_r.publish('chat_server', message)
         self.input_text.clear()
 
+    def send_redis(self):
+        pass
+
     @pyqtSlot(str)
-    def get_text_brower(self,data):
+    def get_text_brower(self, data):
         self.text_browser.append(data)
 
 
